a9ec7fcb2287b05217bbf4b8336f99607f29561a
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-24 00:17:32 +0300

    Create API for GET Post by id request

--- backend/urls.py
+++ backend/urls.py
@@ -21,4 +21,5 @@ from backend import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', views.index),
+    path('api/<int:id>', views.post_detail),
 ]
--- backend/views.py
+++ backend/views.py
@@ -26,3 +26,56 @@ def index(request):
         posts.append(post)
 
     return Response(posts, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def post_detail(request, id):
+    ## Read data from data.json
+    with open("data.json", "r") as f:
+        data = json.load(f)
+
+    ## Find post with given id
+    post = next((post for post in data["posts"] if post["id"] == id), None)
+
+    ## Calculate daily likes for the last 7 days
+    daily_likes = [0, 0, 0, 0, 0, 0, 0]
+    for stat in post['likes']:
+        days_difference = (datetime.now() - datetime.strptime(stat['date'], "%Y-%m-%dT%H:%M:%SZ")).days
+        if days_difference < 7:
+            daily_likes[days_difference] += stat['count']
+
+    ## Calculate daily shares for the last 7 days
+    daily_shares = [0, 0, 0, 0, 0, 0, 0]
+    for stat in post['shares']:
+        days_difference = (datetime.now() - datetime.strptime(stat['date'], "%Y-%m-%dT%H:%M:%SZ")).days
+        if days_difference < 7:
+            daily_shares[days_difference] += stat['count']
+
+    ## Calculate daily comments for the last 7 days
+    daily_comments = [0, 0, 0, 0, 0, 0, 0]
+    for stat in post['comments']:
+        days_difference = (datetime.now() - datetime.strptime(stat['date'], "%Y-%m-%dT%H:%M:%SZ")).days
+        if days_difference < 7:
+            daily_comments[days_difference] += stat['count']
+        
+    ## Extract post details and calculate total likes, shares and comments
+    scheduled = datetime.utcnow() < datetime.strptime(post["schedule"], "%Y-%m-%dT%H:%M:%SZ")
+    post = {
+        "id": post["id"],
+        "title": post["title"],
+        "author": post["author"],
+        "description": post["description"],
+        "imageUrl": post["imageUrl"],
+        "last_seven_days_interactions": {
+            "likes": daily_likes,
+            "shares": daily_shares,
+            "comments": daily_comments,
+        },
+        "likes": sum(entry["count"] for entry in post["likes"]),
+        "shares": sum(entry["count"] for entry in post["shares"]),
+        "comments": sum(entry["count"] for entry in post["comments"]),
+        "status": ['draft'] if post["draft"] else ['scheduled' if scheduled else 'published', post["schedule"]],
+    }
+    if post is None:
+        return Response({"error": "Post not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    return Response(post, status=status.HTTP_200_OK)
