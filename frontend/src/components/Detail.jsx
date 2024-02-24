d2ee1c551bd8e7f1074f3139c8920cd52315587c
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-24 03:46:03 +0300

    Create Detail Page with API GET request

--- frontend/src/components/Detail.jsx
+++ frontend/src/components/Detail.jsx
@@ -1,10 +1,43 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
   const { id } = useParams();
+  const URL = "http://127.0.0.1:8000/api/";
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(`${URL}${id}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log("Something went wrong");
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Detail - {id}</h1>
+    <div className="container">
+      <h1>Post Detail</h1>
+      <div>
+        <h2>{post.title}</h2>
+        <p>{post.author}</p>
+      </div>
+      <div>
+        Chart
+      </div>
+      <div>
+        <p>{post.description}</p>
+        <img src={post.imageUrl} alt={post.title} />
+      </div>
+
     </div>
   );
 };
